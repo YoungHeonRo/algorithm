5e05c1b58ea04446ce8ce2d882c0f997f47f4a48
yhnr15 <yhnr1115@gmail.com>
2018-09-29 21:47:57 +0900

    Update Daejeon16_G_침투.cpp

--- acm-icpc/Daejeon16_G_침투.cpp
+++ acm-icpc/Daejeon16_G_침투.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ios>
 using namespace std;
 
 int **map;
@@ -8,22 +9,22 @@ int row, col;
 bool success = false;
 
 void goWith(int i, int j) {
-	if (i == row-1) success = true;
-	if (map[i][j]==0 && gone[i][j] == 0)
+	if (i == row - 1) success = true;
+	if (map[i][j] == 0 && gone[i][j] == 0)
 	{
 		gone[i][j] = 1;
 		//cout << "(" << i <<"," << j << ")" << endl;
-		if(i-1>0) goWith(i-1, j);
-		if(i+1<col) goWith(i+1, j);
-		if(j-1>0) goWith(i, j-1);
-		if(j+1<row) goWith(i, j+1);
+		if (i - 1>0) goWith(i - 1, j);
+		if (i + 1<row) goWith(i + 1, j);
+		if (j - 1>0) goWith(i, j - 1);
+		if (j + 1<col) goWith(i, j + 1);
 	}
 }
 
 void answer() {
 
 	for (int j = 0; j < col; j++) {
-		if (map[0][j]==0)
+		if (map[0][j] == 0)
 		{
 			goWith(0, j);
 		}
@@ -32,9 +33,12 @@ void answer() {
 }
 
 
-void main(void) {
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
 	string s;
 	cin >> row >> col;
+	cin.ignore();
 
 	map = new int*[row];
 	gone = new int*[row];
@@ -44,29 +48,28 @@ void main(void) {
 	}
 
 	for (int i = 0; i < row; i++) {
-		cin >> s;
+		getline(cin, s);
 		for (int j = 0; j < col; j++) {
-			map[i][j] = int(s[j]-'0');
+			map[i][j] = int(s[j] - '0');
 			gone[i][j] = 0;
 		}
-		cout << endl;
 	}
 	/*
 	cout << "MAP :" << endl;
 	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			cout << map[i][j];
-		}
-		cout << endl;
+	for (int j = 0; j < col; j++) {
+	cout << map[i][j];
+	}
+	cout << endl;
 	}
 	*/
 	answer();
-	if (success == true) cout << "YES";
-	else cout << "NO";
+	if (success == true) cout << "YES" <<endl;
+	else cout << "NO" <<endl;
 
 	for (int i = 0; i < row; i++) {
 		delete[] map[i];
 	}
 	delete[] map;
-
+	return 0;
 }
